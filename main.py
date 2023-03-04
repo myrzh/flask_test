86ed2dad5a08b92cae82681618e90fc36fae2976
Ivan Shtarev <ishtarev@outlook.com>
2023-03-04 21:37:22 +0500

    update gitignore

--- main.py
+++ main.py
@@ -15,28 +15,11 @@ def index():
 
 @app.route("/bootstrap_sample")
 def bootstrap():
-    return """<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
-                    crossorigin="anonymous">
-                    <title>Привет, Яндекс!</title>
-                  </head>
-                  <body>
-                    <h1>Привет, Яндекс!</h1>
-                    <div class="alert alert-primary" role="alert">
-                      А мы тут компонентами Bootstrap балуемся
-                    </div>
-                  </body>
-                </html>"""
+    return render_template("bootstrap_sample.html", title="Bootstrap sample")
 
 
 def main():
-    app.run(port=8000, host="192.168.0.152")
+    app.run(port=8000, host="localhost")
 
 
 if __name__ == "__main__":
